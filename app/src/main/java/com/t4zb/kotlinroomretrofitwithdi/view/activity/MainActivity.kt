0065b73dfa4ac684e5e77e528501d70705a8eaca
beytullahTopuz <beytullah.topuz37@gmail.com>
2024-01-01 13:32:53 +0300

    project structure initialized

---
+++ app/src/main/java/com/t4zb/kotlinroomretrofitwithdi/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+ package com.t4zb.kotlinroomretrofitwithdi.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.t4zb.kotlinroomretrofitwithdi.R
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
